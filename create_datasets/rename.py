085b7f1d3c5af9f3c2a4b5410fa07312fb81a8ec
Mohammad Havaei <seyed.mohammad.havaei@usherbrooke.ca>
2014-12-29 18:27:48 -0500

    added a description for rename.py

--- create_datasets/rename.py
+++ create_datasets/rename.py
@@ -4,12 +4,16 @@ from os.path import join
 from os.path import isdir
 import pdb
 
+'''
+This script changes the directory name from 000x to HG_000x. you can also change the HG to LG to preform the same process on LG dicrectories
+'''
+
 pdb.set_trace()
 list_dir = [d for d in os.listdir('.') if isdir(join('.',d))]
 
 for name in list_dir:
     old_name = join('.',name)
-    d_name = 'LG_' + name
+    d_name = 'HG_' + name
     new_name = join('.',d_name)
     command = 'mv ' + old_name + ' ' + new_name
     os.system(command)
